4cdeb6565c256d8cdadc5535b1a4907a0cf25c8c
Karel Rojas <karelrojas64@gmail.com>
2022-03-02 21:55:16 -0500

    Initial commit, connects to Parse

---
+++ app/src/main/java/com/example/parsetagram/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.example.parsetagram;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUser;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUser = findViewById(R.id.etUser);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick Login button");
+                String user = etUser.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(user, password);
+            }
+        });
+    }
+
+    private void loginUser(String user, String password){
+        Log.i(TAG, "Trying to login user: " + user);
+        // Send to main activity once verified
+        ParseUser.logInInBackground(user, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Problem logging in");
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT);
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+}
