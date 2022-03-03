30221ad8da174cf0ecfa3f7ffb0dbfb321d6f4bd
Karel Rojas <karelrojas64@gmail.com>
2022-03-03 17:00:48 -0500

    Can retrieve posts from back4app

--- app/src/main/java/com/example/parsetagram/MainActivity.java
+++ app/src/main/java/com/example/parsetagram/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.parsetagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btnTakePhoto;
+    private ImageView ivPost;
+    private Button btnSubmit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.etDescription);
+        btnTakePhoto = findViewById(R.id.btnTakePhoto);
+        ivPost = findViewById(R.id.ivPost);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        queryPosts();
+    }
+
+    private void queryPosts(){
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Problem getting posts", e);
+                    return;
+                }
+                for (Post post : posts){
+                    Log.i(TAG, "Post: " + post.getDescription() + ", user: " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/parsetagram/ParseApplication.java
+++ app/src/main/java/com/example/parsetagram/ParseApplication.java
@@ -3,12 +3,15 @@ package com.example.parsetagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
